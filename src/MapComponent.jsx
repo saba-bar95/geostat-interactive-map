c0268900e6ab6d5b7ba39745624f7ca10cc434ff
saba-bar95 <s.barbakadze95@gmail.com>
2025-01-21 11:35:37 +0400

    remove some unnecessary files

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -3,14 +3,16 @@ import "leaflet/dist/leaflet.css";
 import "./map.css";
 
 const MapComponent = () => {
+  const center = [41.697102, 44.773674];
+
   return (
     <>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={center}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
