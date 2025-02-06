3f4eba268c25db7f262fed881fb28e80ab2379fa
saba-bar95 <s.barbakadze95@gmail.com>
2025-02-06 17:41:58 +0400

    add display of regions

--- src/components/Context/Contexts/BusinessStatistics/Indicator/BusinessIndicator.jsx
+++ src/components/Context/Contexts/BusinessStatistics/Indicator/BusinessIndicator.jsx
@@ -1,13 +1,35 @@
 import "./BusinessIndicator.scss";
 import indicators from "./indicators";
 import years from "./years";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { QueriesContext } from "../../../../../App";
 import fetchTurnoverData from "../../../../../functions/fetchTurnoverData";
+import numIntervals from "../../../../ColorBox/numIntervals";
+import AngleRight from "./AngleRight";
+import AngleDown from "./AngleDown";
 
 const BusinessIndicator = () => {
-  const { setRegData, setMunData, setIndicator, setIndicatorYear } =
-    useContext(QueriesContext);
+  const {
+    setRegData,
+    setMunData,
+    setIndicator,
+    setIndicatorYear,
+    regData,
+    munData,
+    indicator,
+    indicatorYear,
+  } = useContext(QueriesContext);
+
+  const indicatorInfo = numIntervals[indicator].measurement;
+
+  const [selected, setSelected] = useState(null);
+
+  // console.log(regData);
+  // console.log(munData);
+
+  const sortedRegions = regData
+    .filter((region) => region[`w_${indicatorYear}`] !== null) // Exclude regions with null values
+    .sort((a, b) => b[`w_${indicatorYear}`] - a[`w_${indicatorYear}`]); // Sort in descending order
 
   const handleIndicatorChanger = (e) => {
     setIndicator(e.target.value);
@@ -60,7 +82,29 @@ const BusinessIndicator = () => {
         </select>
       </div>
 
-      <div className="container"></div>
+      <div className="container regions-container">
+        <div className="header">
+          <p>რეგიონი</p>
+          <p>{indicatorInfo}</p>
+        </div>
+        <div className="regions">
+          {sortedRegions.map((region) => (
+            <div key={region.region_id} className="paras">
+              {region[`w_${indicatorYear}`] > 0 && (
+                <>
+                  <p>
+                    <AngleRight />
+                    <AngleDown />
+
+                    {region.name_ge}
+                  </p>
+                  <p>{region[`w_${indicatorYear}`].toFixed(1)}</p>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
