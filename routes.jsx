957e702596f926c98bf7f5cc7734d7c736811cfd
saba-bar95 <s.barbakadze95@gmail.com>
2025-06-20 13:46:10 +0400

    add routing

--- routes.jsx
+++ routes.jsx
@@ -1,6 +1,15 @@
+import { Navigate } from "react-router-dom";
+import App from "./src/App";
+
 const routes = [
   {
     path: "/",
+    element: <Navigate to="/ge" replace />,
+  },
+
+  {
+    path: "/:language",
+    element: <App />,
   },
 ];
 
