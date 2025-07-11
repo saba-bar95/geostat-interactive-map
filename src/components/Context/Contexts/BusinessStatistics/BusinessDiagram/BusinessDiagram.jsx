bc023f14567f368f4beb4a0d16a990adce720802
saba-bar95 <s.barbakadze95@gmail.com>
2025-07-11 16:08:49 +0400

    add pieChart

--- src/components/Context/Contexts/BusinessStatistics/BusinessDiagram/BusinessDiagram.jsx
+++ src/components/Context/Contexts/BusinessStatistics/BusinessDiagram/BusinessDiagram.jsx
@@ -1,7 +1,16 @@
 import YearsSelect from "../YearsSelect";
 import IndicatorsSelect from "../IndicatorsSelect";
+import { useContext } from "react";
+import { QueriesContext } from "../../../../../App";
+import { useParams } from "react-router";
+import PieChart from "./PieChart";
 
 const BusinessDiagram = () => {
+  const { regData, selectedQuery, indicator, indicatorInfo, indicatorYear } =
+    useContext(QueriesContext);
+
+  const { language } = useParams();
+
   return (
     <div className="business-indicator">
       <div className="container">
@@ -10,6 +19,17 @@ const BusinessDiagram = () => {
       <div className="container">
         <YearsSelect />
       </div>
+      <div className="diagram-header">
+        <p className="diagram-p">
+          {selectedQuery[`title_${language}`]}: {indicator} (
+          {indicatorInfo[`measurement_${language}`]})
+        </p>
+        <p className="diagram-p">
+          {language === "en" ? "Year" : "წელი"}{" "}
+          <span className="diagram-span">{indicatorYear}</span>
+        </p>
+      </div>
+      <PieChart data={regData} year={indicatorYear} />
     </div>
   );
 };
