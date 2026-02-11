4fad13fabd86031839c575d6fd3511e94b63f265
saba-bar95 <s.barbakadze95@gmail.com>
2026-02-11 14:15:32 +0400

    seperate monthes array from monthes select component

---
+++ src/components/Context/ConsumerPriceIndex/Indicator/monthes.js
@@ -0,0 +1,16 @@
+const monthes = [
+  { name_en: "January", name_ge: "იანვარი", value: 1 },
+  { name_en: "February", name_ge: "თებერვალი", value: 2 },
+  { name_en: "March", name_ge: "მარტი", value: 3 },
+  { name_en: "April", name_ge: "აპრილი", value: 4 },
+  { name_en: "May", name_ge: "მაისი", value: 5 },
+  { name_en: "June", name_ge: "ივნისი", value: 6 },
+  { name_en: "July", name_ge: "ივლისი", value: 7 },
+  { name_en: "August", name_ge: "აგვისტო", value: 8 },
+  { name_en: "September", name_ge: "სექტემბერი", value: 9 },
+  { name_en: "October", name_ge: "ოქტომბერი", value: 10 },
+  { name_en: "November", name_ge: "ნოემბერი", value: 11 },
+  { name_en: "December", name_ge: "დეკემბერი", value: 12 },
+];
+
+export default monthes;
