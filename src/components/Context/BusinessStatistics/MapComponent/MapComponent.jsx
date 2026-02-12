84ee3580670bde21e1ad0d31c950c499344ef246
saba-bar95 <saba.barb95@gmail.com>
2026-02-12 14:18:04 +0400

    remove marker when one of search params is changed

--- src/components/Context/BusinessStatistics/MapComponent/MapComponent.jsx
+++ src/components/Context/BusinessStatistics/MapComponent/MapComponent.jsx
@@ -60,13 +60,18 @@ const MapComponent = () => {
     setCompaniesData,
     setSelectedFindRegionId,
     setSelectedFormID,
-    setSelectedActivityID
+    setSelectedActivityID,
+    selectedFormID,
+    selectedActivityID,
   } = useContext(QueriesContext);
   const { language } = useParams();
 
   const mapRef = useRef(null);
   const clusterRef = useRef(null);
 
+  useEffect(() => {
+    setSelectedMarker(null);
+  }, [selectedFormID, selectedActivityID, setSelectedFindRegionId]);
 
   useEffect(() => {
     if (!selectedCompany) {
@@ -74,13 +79,12 @@ const MapComponent = () => {
     }
   }, [selectedCompany]);
 
-
-    useEffect(() => {
-      setSelectedMarker(null);
-      setCompaniesData(null)
-      setSelectedFindRegionId(null)
-      setSelectedFormID(null)
-      setSelectedActivityID(null)
+  useEffect(() => {
+    setSelectedMarker(null);
+    setCompaniesData(null);
+    setSelectedFindRegionId(null);
+    setSelectedFormID(null);
+    setSelectedActivityID(null);
   }, [selectedLink]);
 
   useEffect(() => {
